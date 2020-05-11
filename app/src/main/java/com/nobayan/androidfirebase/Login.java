431a43327bdee298cf123d4348296c868919b8a5
ElSoldado0109 <el.soldado0109@gmail.com>
2020-05-11 15:44:03 +0430

    Nothing

--- app/src/main/java/com/nobayan/androidfirebase/Login.java
+++ app/src/main/java/com/nobayan/androidfirebase/Login.java
@@ -1,5 +1,6 @@
 package com.nobayan.androidfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
@@ -14,6 +15,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.nio.channels.IllegalChannelGroupException;
 
@@ -40,6 +47,7 @@ public class Login extends AppCompatActivity {
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         login_btn = findViewById(R.id.login_btn);
+
         callSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,6 +65,102 @@ public class Login extends AppCompatActivity {
                 startActivity(intent,options.toBundle());
 
 
+
+            }
+        });
+
+
+    }
+
+
+
+    private Boolean validateUsername () {
+
+        String val = username.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+
+        if (val.isEmpty()) {
+            username.setError("Field cannot be empty");
+            return false;
+        }else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return false;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view) {
+        //Validate Login Info
+        if (!validateUsername() | !validatePassword()) {
+            return;
+        } else {
+            isUser();
+        }
+    }
+
+    private void isUser() {
+
+        final String userEnteredUsername = username.getEditText().getText().toString().trim();
+        final String userEnteredPassword = password.getEditText().getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+                     String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+                     if(passwordFromDB.equals(userEnteredPassword)){
+
+                         username.setError(null);
+                         username.setErrorEnabled(false);
+
+                         String nameFromDB = dataSnapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                         String usernameFromDB = dataSnapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                         String phoneNoFromDB = dataSnapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
+                         String emailFromDB = dataSnapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+                         Intent intent = new Intent(getApplicationContext(),UserProfile.class);
+
+                         intent.putExtra("name",nameFromDB);
+                         intent.putExtra("username",usernameFromDB);
+                         intent.putExtra("email",emailFromDB);
+                         intent.putExtra("phoneNo",phoneNoFromDB);
+                         intent.putExtra("password",passwordFromDB);
+
+                         startActivity(intent);
+
+                     }
+                     else {
+                         password.setError("Wrong Password");
+                         password.requestFocus();
+                     }
+                }
+                else{
+                    username.setError("No such User exist Bro");
+                    username.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
