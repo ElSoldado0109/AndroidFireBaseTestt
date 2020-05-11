2bbd68a455ca715bd6fe15cb1d03050b1bb55e62
ElSoldado0109 <el.soldado0109@gmail.com>
2020-05-11 13:28:58 +0430

    Nothing

---
+++ app/src/main/java/com/nobayan/androidfirebase/SignUp.java
@@ -0,0 +1,51 @@
+package com.nobayan.androidfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+    //Variables
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //Hooks to all xml elements in activity_sign_up.xml
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.reg_btn);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+
+        //onClick
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Write a message to the database
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                reference.setValue("Hello");
+
+            }
+        });//reg Btn method end
+    }//end oncreate
+}
